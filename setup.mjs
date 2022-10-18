7a43d6bdba641abbec99789d29a632cf0c91caa2
Wes Cook <im@wescook.ca>
2022-10-18 04:38:22 -0700

    Adding button to toolbar

--- setup.mjs
+++ setup.mjs
@@ -1,7 +1,21 @@
-export function setup({ onCharacterLoaded }) {
-	console.log("Main game is loaded");
-
+export function setup({ onCharacterLoaded, onInterfaceReady }) {
 	onCharacterLoaded(ctx => {
 		console.log("Character is loaded");
 	})
+
+	onInterfaceReady(ctx => {
+		ui.create(
+			NotebookButton({ iconPath: ctx.getResourceUrl('assets/notebook-icon.png') }), document.getElementById('page-header-user-dropdown').parentNode.parentNode
+		);
+	});
+}
+
+function NotebookButton(props) {
+	return {
+		$template: '#notebook-button',
+		iconPath: props.iconPath,
+		openNotebook() {
+			console.log("Button clicked!");
+		}
+	};
 }
