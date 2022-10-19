60435aa8a46fb64cc9eb5657b042ecd7098acd8c
Wes Cook <im@wescook.ca>
2022-10-19 05:50:30 -0700

    Support for light and dark mode icons

--- setup.mjs
+++ setup.mjs
@@ -1,18 +1,30 @@
 export function setup({ onCharacterLoaded, onInterfaceReady }) {
 	onCharacterLoaded(ctx => {
-		let version = 2;
+		let version = 3;
 		console.log("Handy Dandy Notebook : v" + version);
 	})
 
 	onInterfaceReady(ctx => {
-		ui.create(NotebookButton({ iconPath: ctx.getResourceUrl('assets/notebook-icon.png') }), document.getElementById("page-header-user-dropdown").parentNode.parentNode);
+		// Making icon available for CSS
+		// Uses custom properties which are resolved to their blob URIs here,
+		// then set as the background image in styles.css.  Not my first choice
+		// of implementation, but <picture> tags can't change their source based
+		// on selectors (in this case body.darkMode), and data attributes are
+		// out because attr() doesn't work with url() objects until CSS level 5.
+		document.head.insertAdjacentHTML("beforeend", `<style>
+		.notebook {
+			--icon-light: url("${ctx.getResourceUrl("assets/notebook-icon-light.png")} ");
+			--icon-dark: url("${ctx.getResourceUrl("assets/notebook-icon-dark.png")}");
+		}
+		</style>`);
+
+		ui.create(NotebookButton(), document.getElementById("page-header-user-dropdown").parentNode.parentNode);
 	});
 }
 
-function NotebookButton(props) {
+function NotebookButton() {
 	return {
 		$template: '#notebook-button',
-		iconPath: props.iconPath,
 		openNotebook() {
 			document.getElementById("notebook").blur();
 			Swal.fire({
